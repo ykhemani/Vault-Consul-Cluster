07bfe8e20ab26799cd3fb17ec6561c1b5ad541ae
Yash Khemani <yash@hashicorp.com>
2019-05-30 06:38:01 +0000

    remove routes in peering for now

--- terraform-cluster/peering.tf
+++ terraform-cluster/peering.tf
@@ -54,18 +54,18 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
 ####Create Necessary Routes 
 
 # Create a route on Secondary
-resource "aws_route" "r-p" {
-  count                     = "${var.cluster == "Secondary" ? 1 : 0}"
-  route_table_id            = "${element(module.vpc.public_route_table_ids, 0)}"
-  destination_cidr_block    = "10.0.0.0/16"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
-}
+#resource "aws_route" "r-p" {
+#  count                     = "${var.cluster == "Secondary" ? 1 : 0}"
+#  route_table_id            = "${element(module.vpc.public_route_table_ids, 0)}"
+#  destination_cidr_block    = "10.0.0.0/16"
+#  vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
+#}
 
 # Create a route on Primary
-resource "aws_route" "r-s" {
-  count                     = "${var.cluster == "Secondary" ? 1 : 0}"
-  provider                  = "aws.peer"
-  route_table_id            = "${element(data.terraform_remote_state.primary_vault.pub_route_table_id,0)}"
-  destination_cidr_block    = "10.1.0.0/16"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
-}
+#resource "aws_route" "r-s" {
+#  count                     = "${var.cluster == "Secondary" ? 1 : 0}"
+#  provider                  = "aws.peer"
+#  route_table_id            = "${element(data.terraform_remote_state.primary_vault.pub_route_table_id,0)}"
+#  destination_cidr_block    = "10.1.0.0/16"
+#  vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
+#}
